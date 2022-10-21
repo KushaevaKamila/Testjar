f3b9ca716daf5d21407837fadcd742724840128b
Камила <kam20021710@gmail.com>
2022-10-21 21:18:14 +0300

    123

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import ru.kkushaeva.Converter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Converter cnv = new Converter(12.5, 12.6, -5.0, 5.8, 100, 100);
+    }
+}
